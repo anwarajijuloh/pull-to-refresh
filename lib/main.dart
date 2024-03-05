73cec41394cd4818a2e7e9d35fd89202096d6301
Anwar Aji <96333744+anwarajijuloh@users.noreply.github.com>
2024-03-05 22:56:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,235 @@
+import 'package:easy_refresh/easy_refresh.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+import 'skeleton_item.dart';
+
+const kDefaultArcheryTriggerOffset = 200.0;
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pull Refresh',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.green, brightness: Brightness.dark),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: HalloweenPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class HalloweenPage extends StatefulWidget {
+  const HalloweenPage({super.key});
+
+  @override
+  State<HalloweenPage> createState() => _HalloweenPageState();
+}
+
+class _HalloweenPageState extends State<HalloweenPage> {
+  int _count = 10;
+  late EasyRefreshController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = EasyRefreshController(
+      controlFinishRefresh: true,
+      controlFinishLoad: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EasyRefresh(
+        controller: _controller,
+        header: const HalloweenHeader(
+          position: IndicatorPosition.locator,
+          processedDuration: Duration(seconds: 1),
+        ),
+        onRefresh: () async {
+          await Future.delayed(const Duration(seconds: 2));
+          if (!mounted) {
+            return;
+          }
+          setState(() {
+            _count = 10;
+          });
+          _controller.finishRefresh();
+          _controller.resetFooter();
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              leading: Padding(
+                padding: const EdgeInsets.only(left:20.0, top: 10,bottom: 10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  child: Image.asset('assets/myphoto.jpg',fit: BoxFit.cover,),
+                ),
+              ),
+              title: const Text('Welcome, Anwar'),
+              pinned: true,
+            ),
+            const HeaderLocator.sliver(),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return const SkeletonItem();
+                },
+                childCount: _count,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HalloweenHeader extends Header {
+  const HalloweenHeader({
+    super.clamping = false,
+    super.triggerOffset = kDefaultArcheryTriggerOffset,
+    super.position = IndicatorPosition.above,
+    super.processedDuration = Duration.zero,
+    super.springRebound = false,
+    super.hapticFeedback = false,
+    super.safeArea = false,
+    super.spring,
+    super.readySpringBuilder,
+    super.frictionFactor,
+    super.infiniteOffset,
+    super.hitOver,
+    super.infiniteHitOver,
+  });
+
+  @override
+  Widget build(BuildContext context, IndicatorState state) {
+    return _HalloweenIndicator(
+      state: state,
+      reverse: state.reverse,
+    );
+  }
+}
+
+class _HalloweenIndicator extends StatefulWidget {
+  /// Indicator properties and state.
+  final IndicatorState state;
+
+  /// True for up and left.
+  /// False for down and right.
+  final bool reverse;
+
+  const _HalloweenIndicator({
+    Key? key,
+    required this.state,
+    required this.reverse,
+  }) : super(key: key);
+
+  @override
+  State<_HalloweenIndicator> createState() => _HalloweenIndicatorState();
+}
+
+class _HalloweenIndicatorState extends State<_HalloweenIndicator> {
+  double get _offset => widget.state.offset;
+  IndicatorMode get _mode => widget.state.mode;
+  double get _actualTriggerOffset => widget.state.actualTriggerOffset;
+
+  SMINumber? pull;
+  SMITrigger? advance;
+  StateMachineController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.state.notifier.addModeChangeListener(_onModeChange);
+    _loadRiveFile();
+  }
+
+  RiveFile? _riveFile;
+  void _loadRiveFile() {
+    rootBundle.load('assets/pull_to_refresh_use_case.riv').then(
+      (data) async {
+        // Load the RiveFile from the binary data.
+        setState(() {
+          _riveFile = RiveFile.import(data);
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    widget.state.notifier.removeModeChangeListener(_onModeChange);
+    controller?.dispose();
+    // _riveFile = null;
+    super.dispose();
+  }
+
+  /// Mode change listener.
+  void _onModeChange(IndicatorMode mode, double offset) {
+    // print(mode);
+    switch (mode) {
+      case IndicatorMode.drag:
+        controller?.isActive = true;
+      case IndicatorMode.ready:
+        advance?.fire();
+      case IndicatorMode.processed:
+        advance?.fire();
+      default:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_mode == IndicatorMode.drag || _mode == IndicatorMode.armed) {
+      final percentage = (_offset / _actualTriggerOffset).clamp(0.0, 1.1) * 100;
+      pull?.value = percentage;
+    }
+    return SizedBox(
+      width: double.infinity,
+      height: _offset,
+      child: (_offset > 0 && _riveFile != null)
+          ? RiveAnimation.direct(
+              _riveFile!,
+              artboard: 'Bullseye',
+              fit: BoxFit.cover,
+              onInit: (artboard) {
+                controller = StateMachineController.fromArtboard(
+                    artboard, 'numberSimulation')!;
+                controller?.isActive = false;
+                if (controller == null) {
+                  throw Exception(
+                      'Unable to initialize state machine controller');
+                }
+                artboard.addController(controller!);
+                pull = controller!.findInput<double>('pull') as SMINumber;
+                advance = controller!.findInput<bool>('advance') as SMITrigger;
+              },
+            )
+          : const SizedBox.shrink(),
+    );
+  }
+}
